f7d9f86daed489805f9ef5b76430a5b18c7c0098
Raien Shaheen <raienshaheen@gmail.com>
2022-11-26 14:07:07 -0500

    commit tree 46cc490b72d14ee95fe730fffe80b172078cc7ed

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if(someNumber === 500){
+    return 'Yay  500 ft'
+  } else {
+    return 'Sorry try again'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(Name){
+  if (Name === 'new york'){
+    return ' East Side!'
+  } else if(typeof Name === 'string'){
+    return 'Get outta Here $(Name)!'
+  } else {
+    return 'Wait your turn'
+  }
+  }
+  
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(advice){
+  if (advice === 'relax'){
+    return 'Take it easy'
+  }
 }
